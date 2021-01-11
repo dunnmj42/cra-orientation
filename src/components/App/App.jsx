54206da68406fb48cc958471d1df92b96f30516c
Michael Dunn <dunnmj42@gmail.com>
2021-01-11 14:40:45 -0600

    classwork

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Instructions from '../Instructions/Instructions.jsx';
 
+// component - function that returns (renders) JSX
+// css class is className
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World
         </p>
+        <Instructions/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/Instructions/Instructions.jsx
@@ -0,0 +1,13 @@
+function Instructions() {
+    
+    // comment
+    return (
+        <>
+            {/* comment */}
+            <p>Follow the errors</p>
+            <p>say what?</p>
+        </>
+    );
+}
+
+export default Instructions;
